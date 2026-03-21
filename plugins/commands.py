8dab7625d8e37ffc9f80669390e977fdaac53dce
anujofficial9719-eng <anujofficial9719@gmail.com>
2026-03-21 19:53:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -40,6 +40,7 @@ async def start(client, message):
     jishubotz = await message.reply_sticker("CAACAgUAAxkBAAECEEBlLA-nYcsWmsNWgE8-xqIkriCWAgACJwEAAsiUZBTiPWKAkUSmmh4E")
     await asyncio.sleep(2)
     await jishubotz.delete()
+    text = Translation.START_TXT.format(user.mention)
     await message.reply_photo(
     photo=Translation.PHOTO_URL,
     caption=text,
