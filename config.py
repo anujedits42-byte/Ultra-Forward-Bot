1eacab8f80edc2e27cdfbbd505de5ea5cb0e6ad3
anujedits42-byte <anujedits42@gmail.com>
2026-04-07 20:09:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config:
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '7892805795').split()]
 
      # Ensure PORT is an integer for Gunicorn/Web server
-    PORT = int(environ.get('PORT', '5000'))
+    PORT = int(os.environ.get('PORT', '5000'))
 
 
 class temp(object): 
