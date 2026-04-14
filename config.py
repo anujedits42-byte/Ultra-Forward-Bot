158b4a9032e0199d86ce79fd8cb003b628f76778
anujedits42-byte <anujedits42@gmail.com>
2026-04-14 13:57:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,26 +1,28 @@
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
+import datetime
+from os import environ 
 
-
-
-import os
+#Dont Remove My Credit @Silicon_Bot_Update 
+#This Repo Is By @Silicon_Official 
+# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
 
 class Config:
-    API_ID = os.environ.get("API_ID", "34724970")
-    API_HASH = os.environ.get("API_HASH", "f240eae7c60e8e30c17203ab0e052f7e")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8656231605:AAGVZvUNt7VJiqq1r7MT_wbhCLO229ztU2I") 
-    BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
-    DB_NAME = os.environ.get("DB_NAME", "Anujedit")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '7892805795').split()]
+    API_ID = environ.get("API_ID", "34446649")
+    API_HASH = environ.get("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "8779354652:AAEKi1dY-JkMz_WVWLSLD3qduSwIQcbyqrU") 
+    BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Anujedit")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7892805795').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003475522251'))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "anujeditbyak") 
+    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "False")
+    PORT = environ.get('PORT', '8081')
+    
+#Dont Remove My Credit @Silicon_Bot_Update 
+#This Repo Is By @Silicon_Official 
+# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
 
-
-
-
-
+   
 class temp(object): 
     lock = {}
     CANCEL = {}
@@ -28,14 +30,6 @@ class temp(object):
     BANNED_USERS = []
     IS_FRWD_CHAT = []
     
-
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
+#Dont Remove My Credit @Silicon_Bot_Update 
+#This Repo Is By @Silicon_Official 
+# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz # Jishu Developer 
